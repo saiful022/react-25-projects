cb268b65fbc3bc02080f4a212168cb1777df0398
Sheikh Saifullah <saifullah@Sheikhs-Mac-mini.local>
2026-03-09 16:19:40 +0600

    modified

--- Accordian/src/components/accordian/index.jsx
+++ Accordian/src/components/accordian/index.jsx
@@ -1,20 +1,28 @@
 //single selection
 //multiple selection
 
-import React from 'react'
+import React, { useState } from 'react'
 import data from './data'
 
 const Accordian = () => {
+  const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(id) {
+    setSelected(id === selected ? null : id);
+  }
   return (
     <div className='wrapper'>
       <div className="accordian">
         {data && data.length > 0 ? (data.map((dataItem) => 
         (
           <div className="item">
-            <div className="title">
+            <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
               <h3>{dataItem.question}</h3>
               <span>+</span>
             </div>
+            {
+              selected === dataItem.id ? <div>{dataItem.answer}</div> : null
+            }
           </div>
         ))) : (<div>No data found !</div>)}
       </div>
